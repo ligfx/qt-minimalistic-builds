dcd036e804e111770cab4d743433bc2d338a03db
ligfx <ligfx@users.noreply.github.com>
2021-09-24 12:12:56 -0700

    Update msvc2019-build5.15.2-static.ps1

--- msvc2019-build5.15.2-static.ps1
+++ msvc2019-build5.15.2-static.ps1
@@ -2,13 +2,26 @@
 # 2. Run powershell.exe from Native Tools cmd.
 # 3. cd to path of qt5-minimalistic-builds repo.
 
-$tools_folder = $pwd.Path + "\tools\"
+# $version_base = "5.15"
+# $version = "5.15.2"
+
+# $qt_sources_url = "https://download.qt.io/official_releases/qt/" + $version_base + "/" + $version + "/single/qt-everywhere-src-" + $version + ".tar.xz.meta4"
+# $qt_archive_file = $qt_sources_url.split('/')[-1] -replace "\.meta4$"
+# $qt_src_base_folder = $pwd.Path + "\qt-everywhere-src-" + $version
+
+# $tools_folder = $pwd.Path + "\tools\"
+# $type = "static"
+# $install_folder = $pwd.Path + "\qt-5.15.2-mscv2017-x86_64"
+# $prefix_folder = $pwd.Path + "\" + $prefix_base_folder
+# $build_folder = $pwd.Path + "\bld"
+
 
 # Download Qt sources, unpack.
 aria2c "https://download.qt.io/official_releases/qt/5.15/5.15.2/single/qt-everywhere-src-5.15.2.tar.xz.meta4"
 7z x "qt-everywhere-src-5.15.2.tar.xz"
-7z x "qt-everywhere-src-5.15.2.tar"
+7z x "qt-everywhere-src-5.15.2.tar" -aoa
 $qt_src_base_folder = $pwd.Path + "\qt-everywhere-src-5.15.2"
+$install_folder = $pwd.Path + "\qt-5.15.2-mscv2017-x86_64"
 ls
 
 # Configure.
@@ -75,4 +88,4 @@ cd "build"
 
 # Create final archive.
 ls
-7z a -t7z "${prefix_base_folder}.7z" "$prefix_folder" -mmt -mx9
+# 7z a -t7z "${prefix_base_folder}.7z" "$prefix_folder" -mmt -mx9
