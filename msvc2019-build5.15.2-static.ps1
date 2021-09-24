8476bab927c00ce8cc6b69cfaa943920475d0533
ligfx <ligfx@users.noreply.github.com>
2021-09-24 11:10:38 -0700

    Update msvc2019-build5.15.2-static.ps1

--- msvc2019-build5.15.2-static.ps1
+++ msvc2019-build5.15.2-static.ps1
@@ -15,19 +15,6 @@ $prefix_base_folder = "qt-" + $version + "-" + $type + "-msvc2019-x86_64"
 $prefix_folder = $pwd.Path + "\" + $prefix_base_folder
 $build_folder = $pwd.Path + "\bld"
 
-# OpenSSL
-# 1.1.1d
-# Install steps:
-#   1. Download, unpack openssl to folder "C:\xx".
-#   2. Install system-wide perl, nasm.
-#   3. Run MVSC command prompt and navigate to "C:\xx" folder.
-#   4. Run "perl Configure VC-WIN64A --prefix=C:\xx --openssldir=C:\xx -static".
-#   5. Manually edit "makefile" and add /GL /MT to compiler flags and /LTCG to linker flags, then run "nmake" and "make install".
-$openssl_base_folder = "c:\Programy\OpenSSL\bin-static"
-$openssl_include_folder = $openssl_base_folder + "\include"
-$openssl_libs_folder = $openssl_base_folder + "\lib"
-$openssl_bin_folder = $openssl_base_folder + "\bin"
-
 # Download Qt sources, unpack.
 $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
@@ -39,20 +26,61 @@ Invoke-WebRequest -Uri $qt_sources_url -OutFile $qt_archive_file
 mkdir $build_folder
 cd $build_folder
 
-& "$qt_src_base_folder\configure.bat" -debug-and-release -opensource -confirm-license -platform win32-msvc2017 -opengl desktop -no-iconv -no-dbus -no-icu -no-fontconfig -no-freetype -qt-harfbuzz -qt-doubleconversion -nomake examples -nomake tests -skip qt3d -skip qtactiveqt -skip qtcanvas3d -skip qtconnectivity -skip qtdatavis3d -skip qtdoc -skip qtgamepad -skip qtgraphicaleffects -skip qtlocation -skip qtnetworkauth -skip qtpurchasing -skip qtquickcontrols -skip qtquickcontrols2 -skip qtremoteobjects -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech -skip qtvirtualkeyboard -skip qtwebview -skip qtscript -skip qtwebengine -mp -optimize-size -D "JAS_DLL=0" -static -feature-relocatable -ltcg -prefix $prefix_folder -openssl-linked -I $openssl_include_folder -L $openssl_libs_folder OPENSSL_LIBS="-lUser32 -lAdvapi32 -lGdi32 -lWS2_32 -lCRYPT32 -llibcrypto -llibssl"
+& "$qt_src_base_folder\configure.bat" -debug-and-release \
+    -opensource -confirm-license \
+    -platform win32-msvc2017 \
+    -list-modules \
+    -opengl desktop \
+    -no-iconv -no-dbus -no-icu -no-fontconfig -no-freetype -qt-harfbuzz \
+    -nomake examples -nomake tests \
+    -skip qt3d \
+    -skip qtactiveqt \
+    -skip qtandroidextras \
+    -skip qtcanvas3d \
+    -skip qtcharts \
+    -skip qtconnectivity \
+    -skip qtdatavis3d \
+    -skip qtdoc \
+    -skip qtgamepad \
+    -skip qtgraphicaleffects \
+    -skip qtlocation \
+    -skip qtlottie \
+    -skip qtmacextras \
+    -skip qtmultimedia \
+    -skip qtnetwork \
+    -skip qtnetworkauth \
+    -skip qtpurchasing \
+    -skip qtquick3d \
+    -skip qtquickcontrols \
+    -skip qtquickcontrols2 \
+    -skip qtquicktimeline \
+    -skip qtremoteobjects \
+    -skip qtscript \
+    -skip qtscxml \
+    -skip qtsensors \
+    -skip qtserialbus \
+    -skip qtserialport \
+    -skip qtspeech \
+    -skip qtsvg \
+    -skip qtvirtualkeyboard \
+    -skip qtwayland \
+    -skip qtwebchannel
+    -skip qtwebengine \
+    -skip qtwebsockets \
+    -skip qtwebview \
+    -skip qtx11extras \
+    -skip qtxml \
+    -skip qtxmlpatterns \
+    -mp \
+    -D "JAS_DLL=0" \
+    -static \
+    -feature-relocatable \
+    -ltcg \
+    -prefix $prefix_folder
 
 # Compile.
 nmake
 nmake install
-
-# Copy OpenSSL.
-cp "$openssl_libs_folder\*" "$prefix_folder\lib\" -Recurse
-cp "$openssl_include_folder\openssl" "$prefix_folder\include\" -Recurse
-
-# Fixup OpenSSL DLL paths and MySQL paths.
-$openssl_libs_folder_esc = $openssl_libs_folder -replace '\\','\\'
-
-gci -r -include "*.prl" $prefix_folder | foreach-object { $a = $_.fullname; (get-content $a).Replace($openssl_libs_folder_esc, '$$$$[QT_INSTALL_LIBS]\\') | set-content $a }
 
 # Create final archive.
 & "$tools_folder\7za.exe" a -t7z "${prefix_base_folder}.7z" "$prefix_folder" -mmt -mx9
